428db757e0ecbfad3431304861a5407d58ba8429
Willem Melis <zilleplus@gmail.com>
2017-05-08 13:30:05 +0200

    added lin model

---
+++ RIP/part1_lin_model.m
@@ -0,0 +1,48 @@
+clc;clear all;close all;
+%% specifications
+
+Rm = 2.6; % Motor Armature Resistance [ohm]
+Kt = 0.00767 ; % Motor Torque Constant [Nm/A]
+Km = 0.00767; % Motor Back EMF Constant [V/(rad/s)]
+Kg = 70; % Motor Gear Ratio  (The output is slower)
+Mum = 0.69; % Motor Efficiency 
+Mug = 0.9; % Gearbox Efficiency 
+Beq = 0.004; % Equivalent Viscous Damping Coefficient [N m s / rad]
+g = 9.81; % Acceleration of the gravity ]m/s^2]
+Jeq = 0.0044; % Arm Inertia [kg m^2] (at the rotating axis!)
+r = 0.145; % Arm Length [m]
+L =0.305; % Rod Length [m] 
+m = 0.210; % Rod Mass [kg]
+
+%% intermediate variables
+a= Jeq + m*r^2;
+b=m*L*r;
+c=(4/3)*m*L^2;
+d=m*g*L;
+E=a*c - b^2;
+G=(Mum*Mug*Kt*Km*Kg^2+Beq*Rm)/Rm;
+%% lin system
+A=[ 
+    0 0 1 0;
+    0 0 0 1;
+    0 b*d/E -c*G/E 0;
+    0 a*d/E -b*G/E 0;
+    ];
+
+B=[ 
+    0;
+    0;
+    c*(Mum*Mug*Kt*Kg)/(Rm*E);
+    b*(Mum*Mug*Kt*Kg)/(Rm*E)
+  ];
+
+C=eye(4);
+D=zeros(4,1);
+%% simulate the model
+sys=ss(A,B,C,D);
+
+figure;
+impulse(ss);
+
+%% save the matrices and the model to use in other matlab files
+save('model.mat','A','B','C','D');
